214fee3bfddb74127bcf65209de4437f9b07392c
Bishu Giri <bishu@BLR.local>
2020-06-01 23:20:10 +0530

    second commit

---
+++ Woe_IV.py
@@ -0,0 +1,21 @@
+# Author: Bishu Giri
+# Last updated: 30th May 2020
+
+#import packages
+import os
+import pandas as pd
+import numpy as np
+import pandas.core.algorithms as algos
+from pandas import Series
+import scipy.stats.stats as stats
+import re
+import traceback
+import string
+
+
+
+# importing the data
+
+df = pd.read_csv("C:/Users/bishu/Desktop/my_codes/Data/binary_target_sample.csv")
+
+
